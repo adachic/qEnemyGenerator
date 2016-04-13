888b13a23312cf021608de5b3da7e2b96f85fa0c
Akinori Adachi <adachi.c.akinori@gmail.com>
2016-04-13 21:38:32 +0900

    スクリプト修正

--- run10.sh
+++ run10.sh
@@ -10,7 +10,8 @@ if [ $MAX_QUEST_ID = '' ] ; then
         exit
 fi
 
-for i in `seq $MIN_QUEST_ID $MAX_EUQST_ID`
+for i in `seq $MIN_QUEST_ID $MAX_QUEST_ID`
 do
+   echo "./run.sh $i"
    ./run.sh $i
 done
